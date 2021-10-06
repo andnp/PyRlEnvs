7481ba78e33f6531414c988a70ad685b0701c569
andy <andnpatterson@gmail.com>
2021-10-06 10:53:50 -0600

    change tagging script to respect version string type

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -33,7 +33,7 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT`
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
-    sed -i -E "s/(version=).+?,/\1$NEW_TAG,/" setup.py
+    sed -i -E "s/(version=).+?,/\1'$NEW_TAG',/" setup.py
     git add setup.py
     git commit -m "updating to version $NEW_TAG"
     echo "Tagged with $NEW_TAG"
