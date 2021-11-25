9904e5f52b8bba9b6e10f38d9e4031d8cef60a0d
github-action <andnpatterson@gmail.com>
2021-11-25 03:38:57 +0000

    updating to version 0.27

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "RlGlue>=0.3",
         "scipy>=1.5.0",
     ],
-    version='0.26',
+    version='0.27',
     license='MIT',
     description='A handful of fast environments for running RL experiments',
     long_description='todo',
