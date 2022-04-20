a2b4636902079071181453e54d3ebeebea9dce94
andy <andnpatterson@gmail.com>
2022-04-20 13:51:07 -0600

    ci: redo our build system

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-export PYTHONPATH=Py2048
+MYPYPATH=./typings mypy -p PyRlEnvs
+
+export PYTHONPATH=PyRlEnvs
 python3 -m unittest discover -p "*test_*.py"
--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='PyRlEnvs',
     url='https://github.com/andnp/PyRlEnvs.git',
     author='Andy Patterson',
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*', 'scripts*']),
+    version='1.0.0',
+    license='MIT',
+    description='A handful of fast environments for running RL experiments',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
     install_requires=[
         "numpy>=1.19.0",
         "numba>=0.53.0",
-        "RlGlue>=0.3",
+        "RlGlue-andnp>=0.3",
         "scipy>=1.5.0",
     ],
-    version='0.27',
-    license='MIT',
-    description='A handful of fast environments for running RL experiments',
-    long_description='todo',
+    extras_require={},
 )
