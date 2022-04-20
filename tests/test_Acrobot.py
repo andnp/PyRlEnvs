dfee9c98fafb630324962ae72f17cea77d3d71b5
andy <andnpatterson@gmail.com>
2022-04-20 13:59:41 -0600

    test: gym changed acrobot definition

--- tests/test_Acrobot.py
+++ tests/test_Acrobot.py
@@ -1,8 +1,6 @@
-from typing import Any
 import unittest
 import numpy as np
 from PyRlEnvs.domains.Acrobot import Acrobot
-import gym
 
 np.random.seed(0)
 
@@ -17,26 +15,26 @@ class TestAcrobot(unittest.TestCase):
         r = env.reward(np.zeros(0), 0, np.zeros(0))
         self.assertEqual(r, -1)
 
-    def test_stateful(self):
-        env = Acrobot(seed=0)
-        gym_env: Any = gym.make('Acrobot-v1')
+    # def test_stateful(self):
+    #     env = Acrobot(seed=0)
+    #     gym_env: Any = gym.make('Acrobot-v1')
 
-        gym_env.seed(0)
-        gym_env._max_episode_steps = np.inf
+    #     gym_env.seed(0)
+    #     gym_env._max_episode_steps = np.inf
 
-        t = False
-        for step in range(5000):
-            if step % 1000 == 0 or t:
-                gym_env.reset()
-                env.start()
-                s_ = gym_env.state
-                env.setState(s_)
+    #     t = False
+    #     for step in range(5000):
+    #         if step % 1000 == 0 or t:
+    #             gym_env.reset()
+    #             env.start()
+    #             s_ = gym_env.state
+    #             env.setState(s_)
 
-            a = np.random.choice(env.actions(np.zeros(0)))
+    #         a = np.random.choice(env.actions(np.zeros(0)))
 
-            r, sp, t = env.step(a)
-            sp_gym, r_gym, t_gym, _ = gym_env.step(a)
+    #         r, sp, t = env.step(a)
+    #         sp_gym, r_gym, t_gym, _ = gym_env.step(a)
 
-            self.assertTrue(np.allclose(sp, sp_gym))
-            self.assertEqual(t, t_gym)
-            self.assertEqual(r, r_gym)
+    #         self.assertTrue(np.allclose(sp, sp_gym))
+    #         self.assertEqual(t, t_gym)
+    #         self.assertEqual(r, r_gym)
