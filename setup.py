665092159196f4a4d60fac4a5f5985e4c581d9f7
github-action <andnpatterson@gmail.com>
2022-04-21 04:03:14 +0000

    bump: version 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -3,26 +3,33 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
-print(find_packages(exclude=['tests*', 'scripts*']))
+print(find_packages(exclude=["tests*", "scripts*"]))
 
 setup(
-    name='PyRlEnvs',
-    url='https://github.com/andnp/PyRlEnvs.git',
-    author='Andy Patterson',
-    author_email='andnpatterson@gmail.com',
-    packages=find_packages(exclude=['tests*', 'scripts*']),
+    name="PyRlEnvs",
+    url="https://github.com/andnp/PyRlEnvs.git",
+    author="Andy Patterson",
+    author_email="andnpatterson@gmail.com",
+    packages=find_packages(exclude=["tests*", "scripts*"]),
     package_data={"PyRlEnvs": ["py.typed"]},
-    version='1.0.0',
-    license='MIT',
-    description='A handful of fast environments for running RL experiments',
+    version="1.1.0",
+    license="MIT",
+    description="A handful of fast environments for running RL experiments",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
-    install_requires=[
-        "numpy>=1.19.0",
-        "numba>=0.53.0",
-        "RlGlue-andnp>=0.3",
-        "scipy>=1.5.0",
-    ],
-    extras_require={},
+    install_requires=["numpy>=1.19.5", "numba>=0.52.0", "scipy>=1.5.4", "rlglue-andnp"],
+    extras_require={
+        "dev": [
+            "build",
+            "commitizen",
+            "mypy>=0.942",
+            "flake8>=4.0.1",
+            "gym>=0.18.0",
+            "pre-commit",
+            "pygame",
+            "pipenv-setup[black]",
+            "twine",
+        ],
+    },
 )
